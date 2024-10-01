c4327ade0b80ecf628b27014a2c1eb583ba09bd5
Minhas2 <103770686+Minhas2@users.noreply.github.com>
2024-10-01 02:56:33 -0700

    lets start

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Footer from './componets/Footer/Footer';
+import Header from './componets/Header/Header';
+
+
+function App() {
+  return (
+<>
+<Header/>
+<Footer/>
+
+
+</>
+  );
+}
+
+export default App;
