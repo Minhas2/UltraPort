9bed0d3c90a1ef4c1c07b13356ff5ddbea1eb9a8
Minhas2 <103770686+Minhas2@users.noreply.github.com>
2024-10-01 03:33:12 -0700

    123

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@
 import './App.css';
 import Footer from './componets/Footer/Footer';
 import Header from './componets/Header/Header';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
 
 
 function App() {
   return (
 <>
 <Header/>
-<Footer/>
+{/* <Footer/> */}
 
 
 </>
