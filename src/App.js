46a604f4d4775b3608e685e8fbb260b1cbe2d86a
Minhas2 <103770686+Minhas2@users.noreply.github.com>
2024-10-02 08:02:12 -0700

    card and home carsouel

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
 
 import './App.css';
+import Card from './componets/Card/Card';
 import Footer from './componets/Footer/Footer';
 import Header from './componets/Header/Header';
 import '@fortawesome/fontawesome-free/css/all.min.css';
+import HomeCarosuel from './componets/Home-carsoeul/HomeCarosuel';
+import { Route, Routes } from 'react-router-dom';
+import LandingPage from './Pages/LandingPage/LandingPage';
 
 
 
 function App() {
   return (
-<>
-{/* <Header/> */}
-<Footer/>
+    <>
+      {/* <Header/> */}
+
+      {/* <Card /> */}
+     
+
+      <Routes>
+        <Route path='/' element={<HomeCarosuel />} />
+        <Route path='/landing-page' element={<LandingPage />} />
+
+      </Routes>
+
+      {/* <Footer/> */}
 
 
-</>
+    </>
   );
 }
 
