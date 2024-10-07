fb6276deda8139af09f493c096a090f2fe8d8b23
Minhas2 <103770686+Minhas2@users.noreply.github.com>
2024-10-07 10:27:25 -0700

    footer pages
    some pages remaining

--- src/App.js
+++ src/App.js
@@ -23,6 +23,18 @@ import PcPourJouer from './Pages/PcPourJouer/PcPourJouer';
 import Wukong from './Pages/Wukong/Wukong';
 import Starfield from './Pages/Starfield/Starfield';
 import Diablo from './Pages/Diablo/Diablo';
+import Minecraft from './Pages/Minecraft/Minecraft';
+import Fortnite from './Pages/Fortnite/Fortnite';
+import CounterStrike from './Pages/CounterStrike/CounterStrike';
+import Promotions from './Pages/Promotions/Promotions';
+import Footer1 from './Pages/Footer1/Footer1';
+import Footer2 from './Pages/Footer2/Footer2';
+import Footer3 from './Pages/Footer3/Footer3';
+import Footer4 from './Pages/Footer4/Footer4';
+import Footer5 from './Pages/Footer5/Footer5';
+import Footer6 from './Pages/Footer6/Footer6';
+import Footer7 from './Pages/Footer7/Footer7';
+import Footer8 from './Pages/Footer8/Footer8';
 
 
 
@@ -55,10 +67,30 @@ function App() {
         {/* static page  */}
 
         <Route path='/corsair-icue-murals' element={<Page11 />} /> {/* This page is already covered by Page 11 */}
+        <Route path='/nvidia-rtx-40-series' element={<Page6 />} /> {/* This page is already covered by Page 11 */}
         <Route path='/pc-pour-jouer' element={<PcPourJouer />} />
         <Route path='/wukong' element={<Wukong />} />
         <Route path='/starfield' element={<Starfield />} />
         <Route path='/diablo' element={<Diablo />} />
+        <Route path='/minecraft' element={<Minecraft />} />
+        <Route path='/fortnite' element={<Fortnite />} />
+        <Route path='/counter-strike' element={<CounterStrike />} />
+        <Route path='/promotions' element={<Promotions />} />
+
+        {/* footer page  */}
+
+        <Route path='/les-frais-de-port' element={<Footer1 />} />
+        <Route path='/livraison' element={<Footer2 />} />
+        <Route path='/paiement' element={<Footer3 />} />
+        <Route path='/ssl' element={<Footer4 />} />
+        {/* <Route path='/configurateur-pc' element={<Footer4/>} /> {/*  Hassan designed this page*/} 
+        <Route path='/pc-gamer/pc-pour-jouer' element={<Footer5 />} />
+        
+        <Route path='/comparatif-cartes-graphiques' element={<Footer6 />} />
+
+        <Route path='/comparatif-processeur' element={<Footer7/>} />   {/* table remain */}
+
+        <Route path='/comparatif-ssd' element={<Footer8/>} />
 
       </Routes>
 
